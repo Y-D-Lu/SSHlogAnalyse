cf70cc2242d524a462e3d9dd7d5f3f0d0cf7af63
Y-D-Lu <e4256318151@gmail.com>
2018-04-09 10:41:18 +0800

    Initial commit

---
+++ login.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+path = "auth_ex.log"
+
+
+def file2df():
+    f = open(path)  # read from version list
+
+    lines = f.readlines()
+    a=0
+    name=[]
+    pwd=[]
+    for auth in lines:
+        au = auth.split('sshlog: ')[1]
+        # well... something stupid occurred: some attempts of login even do not entered a username. Confused...
+        # and some just entered the empty password. Oh, well, assume that a friend of mine never set a password,
+        # no matter what device he uses and what occasion he's in. And now, the grass on his tomb has been meters high
+        # just be joking XD
+        if au.split().__len__()==2:
+            name.append(au.split()[0])
+            pwd.append(au.split()[1])
+    f.close()
+    df = pd.DataFrame({"name":name,"pwd":pwd})
+    return df
